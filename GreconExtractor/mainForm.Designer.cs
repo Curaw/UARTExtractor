561f5052760d2f52edf6dd8d47b835c462aa4372
Curaw <herrkison@gmail.com>
2019-05-23 00:42:30 +0200

    Add files via upload

---
+++ GreconExtractor/mainForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace GreconExtractor
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dgvTable = new System.Windows.Forms.DataGridView();
+            this.headerID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.headerType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.headerValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.svDialog = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(12, 21);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 23);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "Import Table";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(380, 21);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save Table";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dgvTable
+            // 
+            this.dgvTable.AllowUserToAddRows = false;
+            this.dgvTable.AllowUserToDeleteRows = false;
+            this.dgvTable.AllowUserToOrderColumns = true;
+            this.dgvTable.AllowUserToResizeColumns = false;
+            this.dgvTable.AllowUserToResizeRows = false;
+            this.dgvTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.headerID,
+            this.headerType,
+            this.headerValue});
+            this.dgvTable.Location = new System.Drawing.Point(12, 50);
+            this.dgvTable.Name = "dgvTable";
+            this.dgvTable.ReadOnly = true;
+            this.dgvTable.RowHeadersVisible = false;
+            this.dgvTable.Size = new System.Drawing.Size(443, 239);
+            this.dgvTable.TabIndex = 2;
+            // 
+            // headerID
+            // 
+            this.headerID.HeaderText = "ID";
+            this.headerID.Name = "headerID";
+            this.headerID.ReadOnly = true;
+            this.headerID.Width = 170;
+            // 
+            // headerType
+            // 
+            this.headerType.HeaderText = "Type";
+            this.headerType.Name = "headerType";
+            this.headerType.ReadOnly = true;
+            this.headerType.Width = 170;
+            // 
+            // headerValue
+            // 
+            this.headerValue.HeaderText = "Value";
+            this.headerValue.Name = "headerValue";
+            this.headerValue.ReadOnly = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 301);
+            this.Controls.Add(this.dgvTable);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnImport);
+            this.Name = "frmMain";
+            this.Text = "Grecon Extractor";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dgvTable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn headerID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn headerType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn headerValue;
+        private System.Windows.Forms.SaveFileDialog svDialog;
+    }
+}
+
---
+++ GreconExtractor/mainForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GreconExtractor
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            dgvTable.Rows.Add("1427523462145","7355608", "275");
+            dgvTable.Rows.Add("1427523462146","7355608", "278");
+            dgvTable.Rows.Add("1427523462147", "7355608", "290");
+            dgvTable.Rows.Add("1427523462255", "4438759", "275");
+            dgvTable.Rows.Add("1427523462256", "4438759", "275");
+            dgvTable.Rows.Add("1427523462257", "4438759", "275");
+            dgvTable.Rows.Add("1427523462257", "4438759", "302");
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            svDialog.Filter = "CSV-File|*.csv";
+            svDialog.Title = "Save CSV-File";
+
+            if (svDialog.ShowDialog() == DialogResult.OK)
+            {
+                string path = svDialog.FileName;
+                var csv = new StringBuilder();
+                
+                //Hier ueber die Tabelle iterieren
+                foreach (DataGridViewRow row in dgvTable.Rows)
+                {
+                    for(int i = 0; i < row.Cells.Count; i++) {
+                        csv.Append(row.Cells[i].Value);
+                        csv.Append(";");
+                    }
+                    csv.Append(Environment.NewLine);
+                }
+                System.IO.File.WriteAllText(path, csv.ToString());
+                MessageBox.Show(this,"Saved successfully to: \n" + path,"Saving Table",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            //using (var sp = new System.IO.Ports.SerialPort("COM11", 9600, System.IO.Ports.Parity.None, 8, System.IO.Ports.StopBits.One))
+            using (var sp = new System.IO.Ports.SerialPort("COM7", 9600, System.IO.Ports.Parity.None, 8,System.IO.Ports.StopBits.One))
+            {
+                sp.Open();
+                var readData = sp.ReadLine();
+                Console.WriteLine(readData);
+            }
+        }
+    }
+}
